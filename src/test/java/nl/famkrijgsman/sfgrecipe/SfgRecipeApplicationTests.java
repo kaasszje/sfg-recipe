da2f5c819393404f27ca3638f80599a0ed92c05e
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2023-05-29 08:44:27 +0200

    Initial springboot initializor

---
+++ src/test/java/nl/famkrijgsman/sfgrecipe/SfgRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.famkrijgsman.sfgrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
