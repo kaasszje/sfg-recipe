777e231318e279bc32f84b4d0727309eeb9d2570
Martijn Krijgsman <m.s.krijgsman@gmail.com>
2023-06-07 08:45:44 +0200

    Added categoryrepository it test

---
+++ src/test/java/nl/famkrijgsman/sfgrecipe/repositories/CategoryRepositoryIT.java
@@ -0,0 +1,31 @@
+package nl.famkrijgsman.sfgrecipe.repositories;
+
+import nl.famkrijgsman.sfgrecipe.domain.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class CategoryRepositoryIT {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByDescription() {
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("Mexican");
+
+        assertEquals("Mexican", categoryOptional.get().getDescription());
+    }
+}
